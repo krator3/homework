0146507c2485778be0d9938c46c0277d4bd2d6cf
Maxim Ostapenko <128240873+krator3@users.noreply.github.com>
2023-04-08 22:00:38 +0300

    Add files via upload

---
+++ utils/database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+def create_table():
+    with sqlite3.connect('database.db') as connection:
+        cur = connection.cursor()
+        cur.execute(
+            """
+               CREATE TABLE IF NOT EXISTS user(
+               user_id INT PRIMARY KEY,
+               chat_id INT,
+               username TEXT);
+            """
+        )
+        cur.execute(
+            """
+               CREATE TABLE IF NOT EXISTS chat_message(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               user_id INT,
+               chat_id INT,
+               message TEXT);
+            """
+        )
+        cur.execute(
+            """
+               CREATE TABLE IF NOT EXISTS block_list(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               user_id INTEGER,
+               timestamp INTEGER,
+               reason TEXT);
+            """
+        )
+        connection.commit()
